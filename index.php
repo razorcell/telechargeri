1cdc2073f5fe49c1604cc8d63bdc3c6bf2d50870
root <root@pc.(none)>
2012-12-28 15:41:54 +0000

    1st commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// include Yii bootstrap file
+require_once(dirname(__FILE__).'/..//frameworks/yii1.1.12/yii.php');
+
+// create a Web application instance and run
+Yii::createWebApplication()->run();
